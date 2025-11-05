c4b96d55a4acf902ab6c5ee3398bc94a5c93fe63
Mohit Kumar <mks.djcet@gmail.com>
2025-11-05 16:31:11 +0530

    Updated code

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,12 @@
 import express from 'express';
-import { getUser } from '../controllers/userController.js';
+import { getUser,createUser } from '../controllers/userController.js';
 
 const router = express.Router();
 
+//GET API to fetch all users
 router.get('/users', getUser);
+
+//POST API to create a new user
+router.post('/users', createUser);
 
 export default router;
