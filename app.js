d59d90561e9b9718f015d0691ddfdf6e9eddd602
Mohit Kumar <mks.djcet@gmail.com>
2025-11-13 22:07:41 +0530

    Updated code

--- app.js
+++ app.js
@@ -1,42 +1,47 @@
 import express from 'express';
-import { MongoClient } from 'mongodb';
+import session from 'express-session';
 
-// Initialize Express app
 const app = express();
-const port = 3000;
 
-// MongoDB Connection
-const url = "mongodb://localhost:27017";
-const dbName = "studentDB";
-
-//crete a new MongoClient
-const client = new MongoClient(url);
-
-// Middleware to parse JSON
-app.use(express.json());
-
-//Routes
-app.get('/data',async (req , res)=>{
-    try{
-        // Connect to MongoDB
-        await client.connect();
-        console.log("Connected correctly to server");
-
-        const db = client.db(dbName);
-        const collection = db.collection('students');
-
-        // Fetch all documents
-        const data = await collection.find({}).toArray();
-
-        res.json(data);
-
-    }catch(err){
-        console.error(err);
-        res.status(500).send({error: 'An error occurred'});
+app.use(session({
+    secret: "mySecretKey123",
+    resave: false,
+    saveUninitialized: true,
+    cookie:{
+        maxAge: 10000 * 60 * 60, // 1 hour
+        httpOnly: true
     }
-})
+}));
 
-// Start the server
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+app.get('/', (req, res) => {
+    res.send("<h2>Welcome to the Home Page</h2>");
+});
+
+// Create a session
+app.get("/login", (req, res) => {
+    req.session.user = "Mohit";
+    res.send("Session created for user Mohit");
+});
+
+//Protecting a route
+app.get("/dashboard",(req,res)=>{
+    if(req.session.user){
+        res.send(`Welcome to your dashboard, ${req.session.user}`);
+    } else {
+        res.send("Please login to access the dashboard");
+    }
+});
+
+// Destroying a session
+app.get("/logout",(req,res)=>{
+    req.session.destroy((err)=>{
+        if(err){
+            return res.send("Error in logging out");
+        } 
+        res.send("You have been logged out successfully");
+    });
+});
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
 });
